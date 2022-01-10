a6d9c2de74818395c399271fad099701e5d852a8
Sean Kincaid <63951344+sean-in-dezine@users.noreply.github.com>
2022-01-10 06:55:36 -0800

    deployment 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const moment = require('moment')
 
 
 const app = express()
-const port = 3333
+const port = 8080
 app.use(morgan('dev'))
 app.listen(port, console.log(`port listening on ${port}`.red))
 app.use(express.static(__dirname + "/public"));
